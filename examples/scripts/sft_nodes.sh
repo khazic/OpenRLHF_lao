1a0f5b120dba1bba00303e277e86c88ad3d0b8ec
khazic <khazzz1c@gmail.com>
2025-08-20 16:44:54 +0800

    repuest IB

--- examples/scripts/sft_nodes.sh
+++ examples/scripts/sft_nodes.sh
@@ -16,13 +16,10 @@ export WANDB_API_KEY="9c69c18b00c7dac67189f39e261a257ebd476cda"
 export WANDB_MODE=offline
 
 export NCCL_DEBUG=INFO
-export TOKENIZERS_PARALLELISM=true
-export NCCL_IB_DISABLE=1
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
 export NCCL_SOCKET_IFNAME=eth0
-export NCCL_TIMEOUT=1800
-export NCCL_SHM_DISABLE=0
-export NCCL_SOCKET_FAMILY=IPv4
-export GLOO_SOCKET_IFNAME=eth0
+export NCCL_DEBUG_SUBSYS=ALL
 export MASTER_PORT=29500
 export WORLD_SIZE=16  
 export LOCAL_RANK=0   
@@ -41,7 +38,7 @@ deepspeed --hostfile hostfile.txt \
           --train_batch_size 4096 \
           --micro_train_batch_size 1 \
           --max_samples 9000000 \
-          --pretrain /llm-align/duyimin/duyimin/open_modle/Qwen2.5-7B-8Langs-CPT-250819/ \
+          --pretrain /llm-align/duyimin/duyimin/open_modle/Qwen2.5-7B-8Langs-CPT-250819 \
           --save_path ./checkpoint/Qwen2_5_sft_0820 \
           --save_steps 3000 \
           --logging_steps 3 \
@@ -54,4 +51,6 @@ deepspeed --hostfile hostfile.txt \
           --packing_samples \
           --apply_chat_template \
           --wandb_project "360_Repo" \
-          --wandb_run_name "Qwen2_5_sft_0820"
+          --wandb_run_name "Qwen2_5_sft_0820" \
+          --use_wandb 9c69c18b00c7dac67189f39e261a257ebd476cda 
+
