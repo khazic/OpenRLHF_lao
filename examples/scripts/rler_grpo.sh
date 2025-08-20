91a4b56f4cebd43b927feca0c2205bd615c2e27b
khazic <khazzz1c@gmail.com>
2025-08-20 15:10:52 +0800

    Reconfigured the configuration file

--- examples/scripts/rler_grpo.sh
+++ examples/scripts/rler_grpo.sh
@@ -27,9 +27,9 @@ python3 -m openrlhf.cli.train_ppo_ray \
    --use_kl_loss \
    --kl_estimator k2 \
    --advantage_estimator group_norm \
-   --pretrain /xfr_ceph_sh/liuchonghan/OpenRLHFM/examples/scripts/checkpoint/qwen2_5_sft_domain \
-   --reward_pretrain /xfr_ceph_sh/liuchonghan/OpenRLHFM/examples/scripts/checkpoint/rm-1 \
-   --save_path ./checkpoint/qwen2_5_rler_grpo_2 \
+   --pretrain /xfr_ceph_sh/liuchonghan/checkpoints_set/qwen2_5_sft_domain \
+   --reward_pretrain /xfr_ceph_sh/liuchonghan/checkpoints_set/rm-1 \
+   --save_path ./checkpoint/qwen2_5_rler_grpo_main \
    --ckpt_path ./checkpoint/qwen2_5_rler_grpo_ckpt \
    --save_hf_ckpt \
    --micro_train_batch_size 8 \
@@ -44,7 +44,7 @@ python3 -m openrlhf.cli.train_ppo_ray \
    --zero_stage 3 \
    --bf16 \
    --actor_learning_rate 5e-7 \
-   --prompt_data /xfr_ceph_sh/liuchonghan/OpenRLHFM/grpo_dataset \
+   --prompt_data /xfr_ceph_sh/liuchonghan/grpo_dataset \
    --input_key context_messages \
    --apply_chat_template \
    --normalize_reward \
@@ -53,4 +53,7 @@ python3 -m openrlhf.cli.train_ppo_ray \
    --vllm_sync_backend nccl \
    --enforce_eager \
    --vllm_enable_sleep \
-   --deepspeed_enable_sleep 
+   --deepspeed_enable_sleep \
+   --use_wandb 9c69c18b00c7dac67189f39e261a257ebd476cda \
+   --wandb_project 360_Repo \
+   --wandb_run_name qwen2_5_rler_grpo_main
