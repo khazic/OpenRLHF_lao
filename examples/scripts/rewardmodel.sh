ea129d080ea7d5a97037fa1ed8d586a0dadcd695
khazic <khazzz1c@gmail.com>
2025-08-27 22:18:21 +0800

    kk

--- examples/scripts/rewardmodel.sh
+++ examples/scripts/rewardmodel.sh
@@ -23,28 +23,27 @@ export NCCL_DEBUG_SUBSYS=NONE
 
 read -r -d '' training_commands <<EOF
 openrlhf.cli.train_rm \
-   --save_path ./checkpoint/RewardModel_Qwen2.5_7b \
+   --save_path ./checkpoint/RewardModel_0827_tongyong \
    --save_steps -1 \
    --logging_steps 2 \
-   --eval_steps 20 \
-   --train_batch_size 256 \
-   --micro_train_batch_size 16 \
-   --pretrain /xfr_ceph_sh/liuchonghan/checkpoints_set/qwen2_5_sft_domain \
+   --eval_steps 50 \
+   --train_batch_size 512 \
+   --micro_train_batch_size 8 \
+   --pretrain /xfr_ceph_sh/liuchonghan/OpenRLHF_lao/examples/scripts/checkpoint/SFTmodel_0823 \
    --bf16 \
    --max_epochs 1 \
-   --max_len 4096 \
+   --max_len 8192 \
    --learning_rate 1e-6 \
-   --dataset /xfr_ceph_sh/liuchonghan/reward_dataset \
+   --dataset /xfr_ceph_sh/liuchonghan/tongyong_dataset \
    --apply_chat_template \
    --chosen_key chosen \
    --rejected_key rejected \
-   --load_checkpoint \
    --packing_samples \
    --gradient_checkpointing \
    --value_head_prefix score \
    --use_wandb 9c69c18b00c7dac67189f39e261a257ebd476cda \
    --wandb_project 360_Repo \
-   --wandb_run_name reward_model
+   --wandb_run_name reward_model_tongyong
 EOF
 
 if [[ ${1} != "slurm" ]]; then
