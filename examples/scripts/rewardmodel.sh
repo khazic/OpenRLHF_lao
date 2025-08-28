216ce371e54ae71046865bd6fec3bcdb33288228
khazic <khazzz1c@gmail.com>
2025-08-28 14:00:16 +0800

    kk

--- examples/scripts/rewardmodel.sh
+++ examples/scripts/rewardmodel.sh
@@ -34,7 +34,7 @@ openrlhf.cli.train_rm \
    --max_epochs 1 \
    --max_len 8192 \
    --learning_rate 1e-6 \
-   --dataset /xfr_ceph_sh/liuchonghan/reward_dataset \
+   --dataset /xfr_ceph_sh/liuchonghan/tongyong_dataset \
    --chosen_key chosen \
    --rejected_key rejected \
    --max_samples 1000 \
