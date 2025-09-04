96870be637fd76e7811a2ce9122a1412478fbc15
khazic <khazzz1c@gmail.com>
2025-09-04 13:25:26 +0800

    kk

--- examples/scripts/paper_dapo_k2.sh
+++ examples/scripts/paper_dapo_k2.sh
@@ -14,12 +14,12 @@ fi
 python3 -m openrlhf.cli.train_ppo_ray \
    --ref_num_nodes 1 \
    --ref_num_gpus_per_node 8 \
-   --reward_num_nodes 1 \
-   --reward_num_gpus_per_node 8 \
+   --reward_num_nodes 0 \
+   --reward_num_gpus_per_node 0 \
    --actor_num_nodes 1 \
    --actor_num_gpus_per_node 8 \
    --vllm_num_engines 1 \
-   --vllm_tensor_parallel_size 4 \
+   --vllm_tensor_parallel_size 8 \
    --vllm_gpu_memory_utilization 0.4 \
    --init_kl_coef 1e-3 \
    --gamma 1.0 \
