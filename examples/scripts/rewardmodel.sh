dc5baafe208eacf73e9c10c88a40e1438555545b
khazic <khazzz1c@gmail.com>
2025-09-04 10:38:31 +0800

    0904

--- examples/scripts/rewardmodel.sh
+++ examples/scripts/rewardmodel.sh
@@ -23,12 +23,12 @@ export NCCL_DEBUG_SUBSYS=NONE
 
 read -r -d '' training_commands <<EOF
 openrlhf.cli.train_rm \
-   --save_path ./checkpoint/RewardModel_0904_translate \
+   --save_path ./checkpoint/RewardModel_0904_translate_2 \
    --save_steps -1 \
    --logging_steps 2 \
    --eval_steps 200 \
-   --train_batch_size 64 \
-   --micro_train_batch_size 4 \
+   --train_batch_size 16 \
+   --micro_train_batch_size 2 \
    --pretrain /xfr_ceph_sh/liuchonghan/OpenRLHF_lao/examples/scripts/checkpoint/SFTmodel_0823 \
    --bf16 \
    --max_epochs 1 \
@@ -45,7 +45,7 @@ openrlhf.cli.train_rm \
    --value_head_prefix score \
    --use_wandb 9c69c18b00c7dac67189f39e261a257ebd476cda \
    --wandb_project 360_Repo \
-   --wandb_run_name reward_model_translate_0904
+   --wandb_run_name reward_model_translate_0904_2
 EOF
 
 if [[ ${1} != "slurm" ]]; then
