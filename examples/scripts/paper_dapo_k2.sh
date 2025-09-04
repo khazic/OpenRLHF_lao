2d10907863a0f3bc02c39780972f4103067cd67d
khazic <khazzz1c@gmail.com>
2025-09-04 12:39:59 +0800

    kk

--- examples/scripts/paper_dapo_k2.sh
+++ examples/scripts/paper_dapo_k2.sh
@@ -45,9 +45,6 @@ python3 -m openrlhf.cli.train_ppo_ray \
    --zero_stage 3 \
    --bf16 \
    --apply_chat_template \
-   --enable_new_token_monitoring \
-   --tokenizer_config_path /xfr_ceph_sh/liuchonghan/OpenRLHF_lao/tokenizer_config_added.json \
-   --auto_detect_original_vocab \
    --actor_learning_rate 5e-7 \
    --prompt_data /xfr_ceph_sh/liuchonghan/prompt_dataset \
    --input_key context_messages \
