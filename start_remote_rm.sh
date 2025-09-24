a953fb6ad1a0e664cfdf5c312684ede03a864467
khazic <khazzz1c@gmail.com>
2025-09-24 12:00:17 +0800

    kk

--- start_remote_rm.sh
+++ start_remote_rm.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 echo "Starting Remote Reward Model Server..."
 
-if [ "$CONDA_DEFAULT_ENV" != "openrlhf" ]; then
-    echo "Activating openrlhf environment..."
-    source /xfr_ceph_sh/liuchonghan/envs/etc/profile.d/conda.sh
-    # source /llm-align/liuchonghan/env/etc/profile.d/conda.sh
-    conda activate openrlhf
-fi
-
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export TOKENIZERS_PARALLELISM=false
 
-
-python3 remote_reward_server.py --port 8000 &
+/xfr_ceph_sh/liuchonghan/envs/envs/openrlhf/bin/python remote_reward_server.py --port 8000 &
 
 wait
 
